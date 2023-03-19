53c24debf1e82ca5d707ee71c49d2e2c4c67ce29
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-19 14:26:07 +0100

    Using useEffect and useCallback for request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,7 +8,9 @@ function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
-  async function fetchDataHandler () {
+
+
+  const fetchDataHandler = useCallback(async () => {
     setIsLoading(true)
     setError(null)
 
@@ -35,7 +37,11 @@ function App() {
       setError(error.message)
     }
     setIsLoading(false)
-    }
+    }, [])
+
+    useEffect(() => {
+      fetchDataHandler()
+    }, [fetchDataHandler])
 
     let content = <p>No movies found</p>
     if (movies.length > 0) {
